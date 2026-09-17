f499a99f98eeac0bc3808ee5eeab3c1317a9ba11
deephealingvibes <alessiovisvisentin@gmail.com>
2026-09-18 00:50:08 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE="nature-sounds-v1";
+const CORE=["./","./index.html","./manifest.webmanifest","./icon.svg"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{if(r.ok){const copy=r.clone();caches.open(CACHE).then(x=>x.put(e.request,copy))}return r}).catch(()=>c)))});
